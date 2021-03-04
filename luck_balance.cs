2e24f9ca3136be8f834268a61005f18fa92c379f
Lee GunJun <ac9832@naver.com>
2021-03-04 22:09:31 +0900

    greedy 문제 풀이
    2개 해결

--- luck_balance.cs
+++ luck_balance.cs
@@ -1,22 +1,54 @@
-using System;
+// using System;
+// using System.Collections.Generic;
 
-namespace luck_blance
-{
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            int n = Convert.ToInt32(Console.ReadLine());
-            int k = Convert.ToInt32(Console.ReadLine());
-            int[] L = new int[n];
-            int[] T = new int[n];
+// namespace luck_blance
+// {
+//     class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             string[] str = Console.ReadLine().Split(' ');
+//             int n = Convert.ToInt32(str[0]);
+//             int k = Convert.ToInt32(str[1]);
+//             int[] L = new int[n];
+//             int[] T = new int[n];
+//             List<int> M = new List<int>();
 
-            for(int i = 0; i < n; i++)
-            {
-                string[] inputData = Console.ReadLine();
-                L[i] = Convert.ToInt32(inputData[0]);
-                T[i] = Convert.ToInt32(inputData[1]);
-            }
-        }
-    }
-}
+//             for(int i = 0; i < n; i++)
+//             {
+//                 string[] inputData = Console.ReadLine().Split(' ');
+//                 L[i] = Convert.ToInt32(inputData[0]);
+//                 T[i] = Convert.ToInt32(inputData[1]);
+//             }
+
+//             int calK = 0;
+//             int totalResult = 0;
+//             int mCount = 0;
+//             for(int i = 0; i < n; i++)
+//             {
+//                 if(T[i] == 0)
+//                 {
+//                     totalResult += L[i];
+//                 }
+//                 else
+//                 {
+//                     totalResult += L[i];
+//                     M.Add(L[i]);
+//                     mCount++;
+//                     calK++;
+//                 }
+//             }
+
+//             if(calK > k)
+//             {
+//                 M.Sort();
+//                 for(int i = 0; i < calK - k; i++)
+//                 {
+//                     totalResult -= M[i] * 2;
+//                 }
+//             }
+
+//             Console.WriteLine(totalResult);
+//         }
+//     }
+// }
